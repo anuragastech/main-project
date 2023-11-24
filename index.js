eb0551a65ee19c99ae42ac59f5174bb7d76d19c3
Anurag <anuragraveendren98@gmail.com>
2023-11-24 12:32:30 +0530

    project

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express =require('express');
 const hbs =require ('hbs')
+const path=require('path')
 const  app = express();
-const port =3006;
+const port =3008;
+
+
 
 app.set('view engine', 'hbs');
 
@@ -10,12 +13,15 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-app.use(express.static('views'));
-app.use(express.static('public'));
-app.use(express.static('img'));
-app.use(express.static('font'));
-app.use(express.static('sass'));
-app.use(express.static('source'));
+app.use(express.static(path.join(__dirname,'views')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'js')));
+app.use(express.static(path.join(__dirname,'img')));
+app.use(express.static(path.join(__dirname,'font')));
+app.use(express.static(path.join(__dirname,'saas')));
+
+
+
 
 
 app.get('/sign',(req,res)=>{
@@ -23,13 +29,33 @@ app.get('/sign',(req,res)=>{
 });
 
 
-app.get('/',(req,res)=>{
+app.get('/login',(req,res)=>{
     res.render('login')
 })
 
 app.get('/index',(req,res)=>{
     res.render('index')
 })
+
+app.get('/product-page',(req,res)=>{
+    res.render('product-page')
+})
+
+
+app.get('/categories',(req,res)=>{
+    res.render('categories')
+});
+app.get('/check-out',(req,res)=>{
+    res.render('check-out')
+});
+app.get('/contact',(req,res)=>{
+    res.render('contact')
+});
+
+app.get('/shopping-cart',(req,res)=>{
+    res.render('shopping-cart')
+});
+
 
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
